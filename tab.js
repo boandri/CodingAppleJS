33d1a7c7fe6fb99c38d9c3b304fe6386f659c080
boandri <96402563+boandri@users.noreply.github.com>
2023-06-18 00:38:21 -0400

    created detail.html

---
+++ tab.js
@@ -0,0 +1,9 @@
+// 만약 1번 li 눌리면{
+//     모든 버튼에 붙은 orange class이름 제거,
+//     1번 li에 orange추가,
+//     모든 버튼에 붙은 show class이름 제거,
+//     1번 div에 show 추가}
+
+$('.tab-button').eq(0).on('click', function(){
+
+})
